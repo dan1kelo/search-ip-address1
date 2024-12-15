39317d65a9b8d0b2bfafb37ce26375e9cf100ab2
monarch <maduludaniel1@gmail.com>
2024-12-15 13:56:48 +0100

    Update узнать айпи адрес по сайту.py

--- узнать айпи адрес по сайту.py
+++ узнать айпи адрес по сайту.py
@@ -2,12 +2,12 @@ import socket
 
 def get_ip_by_site(site_url):
     try:
-        # Получение IP-адреса сайта
+        
         ip_address = socket.gethostbyname(site_url)
         return f"IP-адрес сайта {site_url}: {ip_address}"
     except socket.gaierror:
         return f"Не удалось получить IP-адрес для {site_url}."
 
 if __name__ == "__main__":
-    site_url = input("Введите URL сайта (например, google.com): ").strip()
+    site_url = input("Введите URL сайта : ").strip()
     print(get_ip_by_site(site_url))
